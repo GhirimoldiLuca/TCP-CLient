0c2ff3794390c61569521ae1d798028fc98b3ee6
Dom64bit <lucaghir2@gmail.com>
2022-10-07 14:01:08 +0200

    fix errors, javadoc

--- src/Client.java
+++ src/Client.java
@@ -1,58 +1,88 @@
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Scanner;
 
-
+/**
+ * Client process for exchange of Strings through the {@link Socket} with the Server
+ * 
+ * @author Ghirimoldi Luca
+ * 
+ * @version 1.1.3
+ * @since 2022-09-24
+ */
 public class Client implements Interface {
     private int attempts = 0;
-    private Socket client = null;
+    Socket client;
     private InetAddress ip;
-    private static Scanner sc = new Scanner(System.in);
+    private BufferedReader keyboard;
     private DataOutputStream out;
     private BufferedReader in;
-    
-    protected void connect() {
+
+    /**
+     * Create the {@link Socket} @param {@link InetAddress}, @param PORT} 
+     * which try to estabilish the connection with the {@link ServerSocket} 3 times.
+     * 
+     * Handling the {@link UnUnknownHostException} , {@link IOException} 
+     * 
+     * @return {@link Socket} 
+     * 
+     */
+    protected Socket connect() {
         try {
+            this.keyboard = new BufferedReader(new InputStreamReader(System.in));
             this.ip = InetAddress.getLocalHost();
             while (this.attempts < 3 || this.client == null) {
                 this.client = new Socket(ip, PORT);
                 this.attempts++;
             }
-            if(this.attempts == 3 || this.client == null){
+            if (this.client == null) {
                 throw new IOException("Can't estabilish connection with server!");
-            }else {
+            } else {
                 System.out.println("Connected to the server!");
-            }   
+            }
 
             this.out = new DataOutputStream(this.client.getOutputStream());
             this.in = new BufferedReader(new InputStreamReader(this.client.getInputStream()));
         } catch (UnknownHostException e) {
             System.err.println("Unknown local host address");
             System.exit(2);
-        }
-        catch(IOException e){
+        } catch (IOException e) {
             System.err.println(e.getMessage());
             System.exit(3);
         }
-        System.out.print("Enter text to send to the server:");
-            
-        
+
+        return this.client;
     }
-    protected void communicate(){
-        try{
-            this.out.writeBytes(sc.nextLine());
+
+    /**
+     * Send a String to the Server, 
+     * Receive a String containing the count of vowels and consonants of the messages sent to the server
+     */
+    protected void communicate() {
+        try {
+            System.out.print("Enter text to send to the server:");
+            String text = keyboard.readLine();
+            this.out.writeBytes(text + "\n");
+            this.out.flush();
             String received = this.in.readLine();
             System.out.println(received);
+            this.client.close();
         } catch (IOException e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
+    }
+
+    /**
+     * Start the client process
+     */
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.connect();
+        client.communicate();
     }
 
 }
